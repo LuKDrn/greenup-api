2016615c92164637a485694880608584b5f3511b
LuKDrn <derouin.lucas@laposte.net>
2021-07-05 11:32:23 +0200

    Login and Register

--- GreenUpDotnet/GreenUp.Web.Mvc/Controllers/Authentications/AuthController.cs
+++ GreenUpDotnet/GreenUp.Web.Mvc/Controllers/Authentications/AuthController.cs
@@ -1,14 +1,18 @@
 ﻿using GreenUp.Core.Business.Users.Models;
 using GreenUp.EntityFrameworkCore.Data;
 using GreenUp.Web.Core.Controllers;
+using GreenUp.Web.Mvc.Models.Auth;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GreenUp.Web.Mvc.Controllers.Authentications
 {
@@ -20,30 +24,63 @@ namespace GreenUp.Web.Mvc.Controllers.Authentications
         {}
 
         [HttpPost, Route("login")]
-        public IActionResult Login([FromBody]User user)
+        public async Task<IActionResult> Login([FromBody]LoginViewModel model)
         {
-            if (user == null)
+            if (ModelState.IsValid)
             {
-                return BadRequest("Invalid client request");
-            }
-            if (user.Mail == "johndoe@mail.com" && user.Password == "def@123")
-            {
-                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
-                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
-                var tokeOptions = new JwtSecurityToken(
-                    issuer: "https://localhost:5001",
-                    audience: "https://localhost:5001",
-                    claims: new List<Claim>(),
-                    expires: DateTime.Now.AddMinutes(5),
-                    signingCredentials: signinCredentials
-                );
-                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
-                return Ok(new { Token = tokenString });
+                User user = await _context.Users.FirstOrDefaultAsync(u => u.Mail == model.Mail);
+                if (user != null)
+                {
+                    bool verified = BCrypt.Net.BCrypt.Verify(model.Password, user.Password);
+                    if (verified)
+                    {
+                        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                        var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                        var tokeOptions = new JwtSecurityToken(
+                            issuer: "https://localhost:5001",
+                            audience: "https://localhost:5001",
+                            claims: new List<Claim>(),
+                            expires: DateTime.Now.AddMinutes(5),
+                            signingCredentials: signinCredentials
+                        );
+                        var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                        return Ok(new { Token = tokenString });
+                    }
+                    else
+                    {
+                        return Unauthorized("Mail ou Mot de passe incorrecte");
+                    }
+                }
+                else
+                {
+                    return Unauthorized("Mail ou mot de passe incorrecte.");
+                }
             }
             else
             {
-                return Unauthorized();
+                return BadRequest("Model not valid");
             }
+        }
+
+        [HttpPost, Route("register")]
+        public async Task<User> Register([FromBody]RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User()
+                {
+                    Mail = model.Mail,
+                    Password = BCrypt.Net.BCrypt.HashPassword(model.Password),
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    BirthDate = model.BirthDate,
+                    Role = await _context.Roles.Where(r => r.Value == "User").FirstOrDefaultAsync()
+                };
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return user;
+            }
+            throw new Exception("Model not valid");
         }
     }
 }
