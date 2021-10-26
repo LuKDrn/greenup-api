12cd014436621e09c13f08c7c8a4f010d3018b69
Marc-antoine simon <marc-antoine.simon3@orange.fr>
2021-10-26 12:21:30 +0200

    cardMissionComponent : travail en cours

--- GreenUpAngular/src/app/component/card/card.component.ts
+++ GreenUpAngular/src/app/component/card/card.component.ts
@@ -7,42 +7,13 @@ import { MissionComp } from 'src/app/modules/home/home.component';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  // @Input() image: string = '';
-  // @Input() title: string = '';
-  // @Input() location: string = '';
-  // @Input() point: string = '';
-  // @Input() titleDesc: string = ''; 
-  // @Input() desc: string = '';
-  // @Input() date: string = '';
-
   @Input() mission?: MissionComp;
 
   constructor(
     private router: Router
-  ) {
-    // this.mission = {
-    //   image: this.image,
-    //   title: this.title,
-    //   location: this.location,
-    //   point: this.point,
-    //   titleDesc: this.titleDesc, 
-    //   desc: this.desc,
-    //   date: this.date,
-    // }
-  }
+  ) {}
 
   ngOnInit() {
     console.log(this.mission);
   }
-
-}
-
-interface missionComp {
-  image?: string;
-  title?: string;
-  location?: string;
-  point?: string;
-  titleDesc?: string; 
-  desc?: string;
-  date?: string;
 }
