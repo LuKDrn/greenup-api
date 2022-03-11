ef141960a98a25c861011f1043a62fe1e67a6047
Marc-antoine simon <marc-antoine.simon3@orange.fr>
2022-03-11 16:33:02 +0100

    update footer and associations

--- GreenUpAngular/src/app/component/footer/footer.component.ts
+++ GreenUpAngular/src/app/component/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -9,13 +10,22 @@ export class FooterComponent implements OnInit {
 
   public information: string;
 
-  constructor() {
+  constructor(
+    private route: Router
+  ) {
     this.information = 'Protegeons notre maison, engageons-nous dans les petites actions quotidiennes'; 
   }
 
   ngOnInit(): void {
   }
 
+  public goToAPropos(): void {
+    this.route.navigate(['/a-propos']);
+  }
+
+  public goToAccueil(): void {
+    this.route.navigate(['/']);
+  }
 
 
 }
